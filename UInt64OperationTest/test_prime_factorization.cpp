41bb49df9a1fb71b83ea4c4f76e467a1d5d38803
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2020-08-29 10:50:31 +0900

    Test powerOf2

--- UInt64OperationTest/test_prime_factorization.cpp
+++ UInt64OperationTest/test_prime_factorization.cpp
@@ -54,3 +54,29 @@ TEST( TestPrimeFactorization, primeNumber ) {
 	a = PrimeFactorizationType{ { 18446744073709551533, 1 } };
 	EXPECT_EQ( a, PrimeFactorize( 18446744073709551533ULL ) );
 }
+
+TEST( TestPrimeFactorization, powerOf2 ) {
+
+	auto a = PrimeFactorizationType{ { 2, 3 } };
+	EXPECT_EQ( a, PrimeFactorize( 8ULL ) );
+	a = PrimeFactorizationType{ { 2, 4 } };
+	EXPECT_EQ( a, PrimeFactorize( 16ULL ) );
+	a = PrimeFactorizationType{ { 2, 8 } };
+	EXPECT_EQ( a, PrimeFactorize( 256ULL ) );
+	a = PrimeFactorizationType{ { 2, 16 } };
+	EXPECT_EQ( a, PrimeFactorize( 65536ULL ) );
+	a = PrimeFactorizationType{ { 2, 31 } };
+	EXPECT_EQ( a, PrimeFactorize( 2147483648ULL ) );
+	a = PrimeFactorizationType{ { 2, 32 } };
+	EXPECT_EQ( a, PrimeFactorize( 4294967296ULL ) );
+
+	a = PrimeFactorizationType{ { 2, 60 } };
+	EXPECT_EQ( a, PrimeFactorize( 1152921504606846976ULL ) );
+	a = PrimeFactorizationType{ { 2, 61 } };
+	EXPECT_EQ( a, PrimeFactorize( 2305843009213693952ULL ) );
+	a = PrimeFactorizationType{ { 2, 62 } };
+	EXPECT_EQ( a, PrimeFactorize( 4611686018427387904ULL ) );
+	a = PrimeFactorizationType{ { 2, 63 } };
+	EXPECT_EQ( a, PrimeFactorize( 9223372036854775808ULL ) );
+
+}
